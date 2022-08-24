62dd20ac05029772e0f6718172668a16c58ab6e8
ChessMax <ChessMax@yandex.ru>
2022-08-24 21:30:01 +0300

    Simple build command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-﻿console.log('Hello world');
+﻿// #!/usr/bin/env node
+
+const {Command} = require('commander');
+
+const program = new Command()
+    .name('dev-press')
+    .description('Simple static blog generator');
+
+program
+    .command('build', 'Generates static blog')
+    .action(() => {
+        console.log('build');
+    });
+
+
+program.parse();
