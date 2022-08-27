b29eeeb322074baa8ae0c133a96d8067e48960fc
ChessMax <ChessMax@yandex.ru>
2022-08-27 16:57:38 +0300

    The simplest md -> html conversion added.

--- src/commands/build_command.ts
+++ src/commands/build_command.ts
@@ -1,5 +1,25 @@
-﻿export async function buildCommand(): Promise<void> {
-    console.log('build');
-    console.log('build 2');
+﻿import {glob} from "glob";
+import MarkdownIt from "markdown-it";
+import * as fs from "fs";
+import * as path from 'path'
+
+export async function buildCommand(): Promise<void> {
+    let mdi = MarkdownIt({
+        html: true,
+    });
+    let mds = glob.sync('./posts/*.md');
+    for (const md of mds) {
+        console.log(`md: ${md}`);
+
+        let content = fs.readFileSync(md, 'utf8');
+        let html = mdi.render(content);
+
+        let dir = path.dirname(md);
+        let fileName = path.basename(md, '.md');
+
+        let htmlPath = path.join(dir, `${fileName}.html`);
+
+        fs.writeFileSync(htmlPath, html);
+    }
 }
 
