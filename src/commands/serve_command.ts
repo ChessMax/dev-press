d70898f347480e6ea7ca73d9ae532499cad4988e
ChessMax <ChessMax@yandex.ru>
2022-10-15 21:52:58 +0300

    Simple file watching added.

--- src/commands/serve_command.ts
+++ src/commands/serve_command.ts
@@ -1,9 +1,30 @@
 ﻿import e from "express";
 import {buildCommand} from "./build_command";
+import {watch} from "chokidar";
 
 export async function serveCommand(): Promise<void> {
     await buildCommand({
         baseUrlOverride: '',
+    });
+
+    let building = false;
+
+    let watcher = watch('.', {
+        persistent: true,
+        ignoreInitial: true,
+        ignored: ['.*', './public', './node_modules'],
+    }).on('all', async (event, path) => {
+        if (!building) {
+            building = true;
+            console.log(`Something changed: ${event} ${path}`);
+
+            await buildCommand({
+                baseUrlOverride: '',
+            });
+
+            console.log('rebuild completed');
+            building = false;
+        }
     });
 
     const app = e();
